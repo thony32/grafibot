93cea5bee5f5e1ee37dbea6af12866f060b947c4
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-02-01 00:16:22 +0300

    ES6 modules

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-var createError = require("http-errors")
-var express = require("express")
-var path = require("path")
-var cookieParser = require("cookie-parser")
-var logger = require("morgan")
-var indexRouter = require("./routes/index")
-var usersRouter = require("./routes/users")
+"use strict"
 
-var app = express()
+import createError from "http-errors"
+import express from "express"
+import { static as expressStatic, json, urlencoded } from "express"
+import { join } from "path"
+import cookieParser from "cookie-parser"
+import logger from "morgan"
+import indexRouter from "./routes/index"
+import usersRouter from "./routes/users"
+
+const app = express()
 
 app.use(logger("dev"))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(json())
+app.use(urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, "public")))
+app.use(expressStatic(join(__dirname, "public")))
 
 app.use("/", indexRouter)
 app.use("/users", usersRouter)
@@ -33,4 +36,4 @@ app.use(function (err, req, res, next) {
     res.render("error")
 })
 
-module.exports = app
+export default app
